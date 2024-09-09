69be248fd4f054647adb4aa14177bdf72a2abe0f
vasek1 <cistecky121@trebesin.cz>
2024-09-09 13:14:08 +0200

    matura

---
+++ maturitni_hra/button.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Button():
+    def __init__(self,x,y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.height = 800
+        self.width = 1200
+        self.screen = pygame.display.set_mode((self.width, self.height))
+    def draw(self):
+        self.screen.blit(self.image,(self.rect.x, self.rect.y))
---
+++ maturitni_hra/game.py
@@ -0,0 +1,32 @@
+import pygame
+from sys import exit
+import button
+
+
+pygame.init()
+
+
+screen_height = 800
+screen_width =1200
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+start_img = pygame.image.load("image/start_tlacitko.png")
+exit_img= pygame.image.load("image/exi_tlacitko.png")
+start_button = button.Button(100,200, start_img)
+exit_button = button.Button(450,200, exit_img)
+
+while True:
+
+    for event in pygame.event.get():
+     
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        screen.fill((0,0,0))
+        start_button.draw()
+        exit_button.draw()
+
+
+
+    
+        pygame.display.update()
