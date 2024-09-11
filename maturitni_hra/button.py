9644252c1230013b02d0d24a8eecb51f4eed45cc
vasek1 <cistecky121@trebesin.cz>
2024-09-11 12:30:00 +0200

    tlacitka

--- maturitni_hra/button.py
+++ maturitni_hra/button.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Button():
     def __init__(self,x,y, image):
         self.image = image
--- maturitni_hra/game.py
+++ maturitni_hra/game.py
@@ -2,18 +2,20 @@ import pygame
 from sys import exit
 import button
 
-
+clock = pygame.time.Clock()
 pygame.init()
 
-
+background = pygame.image.load("image/menu/background_menu.png")
 screen_height = 800
 screen_width =1200
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+
 start_img = pygame.image.load("image/menu/start_tlacitko.png")
 exit_img= pygame.image.load("image/menu/exit_tlacitko.png")
-start_button = button.Button(400,300, start_img)
-exit_button = button.Button(100,600, exit_img)
+start_button = button.Button(600,300, start_img)
+exit_button = button.Button(600,500, exit_img)
+
 
 while True:
 
@@ -22,7 +24,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        screen.fill((0,0,0))
+        screen.blit(background,(0,0))
         start_button.draw()
         exit_button.draw()
 
