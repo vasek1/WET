0f5cb16180220c08ac54584a69dbd10c814f4607
vasek1 <cistecky121@trebesin.cz>
2024-09-13 16:31:05 +0200

    tlacitka

--- maturitni_hra/button.py
+++ maturitni_hra/button.py
@@ -2,12 +2,19 @@ import pygame
 
 
 class Button():
-    def __init__(self,x,y, image):
-        self.image = image
+    def __init__(self,x,y, image,scale):
+        self.width1 = 46
+        self.height1 = 22
+        self.image = pygame.transform.scale(image,(int(self.width1*scale), int(self.height1*scale)))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x,y)
-        self.height = 800
         self.width = 1200
+        self.height = 800
         self.screen = pygame.display.set_mode((self.width, self.height))
     def draw(self):
         self.screen.blit(self.image,(self.rect.x, self.rect.y))
+    def click(self,event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(pygame.mouse.get_pos()):
+                return True
+        return False
--- maturitni_hra/game.py
+++ maturitni_hra/game.py
@@ -6,17 +6,18 @@ clock = pygame.time.Clock()
 pygame.init()
 
 background = pygame.image.load("image/menu/background_menu.png")
+tutorial_text = pygame.image.load("image/tutorial_s_napisem.png")
 screen_height = 800
 screen_width =1200
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 
-start_img = pygame.image.load("image/menu/start_tlacitko.png")
-exit_img= pygame.image.load("image/menu/exit_tlacitko.png")
-start_button = button.Button(600,300, start_img)
-exit_button = button.Button(600,500, exit_img)
-
-
+start_image = pygame.image.load("image/menu/start_tlacitko.png")
+exit_image= pygame.image.load("image/menu/exit_tlacitko.png")
+start_button = button.Button(416,200, start_image, 8)
+exit_button = button.Button(416,500, exit_image, 8)
+bezi = True
+Tutorial = False
 while True:
 
     for event in pygame.event.get():
@@ -28,7 +29,14 @@ while True:
         start_button.draw()
         exit_button.draw()
 
-
+        if start_button.click(event):
+            Tutorial = True
+            
+        if exit_button.click(event):
+                bezi = False
+                exit()
+        if Tutorial:
+            screen.blit(tutorial_text,(0,0))
 
     
         pygame.display.update()
