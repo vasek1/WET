d976f317353d70121e40004ba394bc6320d9d638
vasek1 <cistecky121@trebesin.cz>
2024-10-06 17:30:45 +0200

    tutorial_hotov

--- maturitni_hra/game.py
+++ maturitni_hra/game.py
@@ -6,7 +6,7 @@ clock = pygame.time.Clock()
 pygame.init()
 
 background = pygame.image.load("image/menu/background_menu.png")
-tutorial_text = pygame.image.load("image/tutorial_s_napisem.png")
+tutorial_text = pygame.image.load("image/tutorial/tutorial_s_napisem.png")
 screen_height = 800
 screen_width =1200
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -14,10 +14,13 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 start_image = pygame.image.load("image/menu/start_tlacitko.png")
 exit_image= pygame.image.load("image/menu/exit_tlacitko.png")
+tutorial_image = pygame.image.load("image/tutorial/skip_tlacitko.png")
+tutorial_button = button.Button(700,690, tutorial_image, 5)
 start_button = button.Button(416,200, start_image, 8)
 exit_button = button.Button(416,500, exit_image, 8)
-bezi = True
+start= True
 Tutorial = False
+Game_go = False
 while True:
 
     for event in pygame.event.get():
@@ -28,15 +31,19 @@ while True:
         screen.blit(background,(0,0))
         start_button.draw()
         exit_button.draw()
+        
 
         if start_button.click(event):
             Tutorial = True
             
         if exit_button.click(event):
-                bezi = False
+                start = False
                 exit()
         if Tutorial:
             screen.blit(tutorial_text,(0,0))
-
-    
+            tutorial_button.draw()
+        if tutorial_button.click(event):
+             Game_go = True
+        if Game_go:
+             screen.blit(background,(0,0))
         pygame.display.update()
