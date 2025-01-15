8fbe4afc0fda476ba7209a75f6013d58aa652342
vasek1 <cistecky121@trebesin.cz>
2025-01-15 10:38:09 +0100

    oprava

--- WET/Programování/web_scraping/web_scraping.py
+++ WET/Programování/web_scraping/web_scraping.py
@@ -5,4 +5,10 @@ import requests
 def main():
     url = "https://www.trebesin.cz"
     response = requests.get(url)
-    soup = BeautifulSoup(html,"html.parser")
+    soup = BeautifulSoup(response.content,"html.parser")
+
+    paragraph = soup.p
+    
+    print(f"První odstavec na stránce je :{paragraph}")
+if __name__ == "__main__":
+    main()
